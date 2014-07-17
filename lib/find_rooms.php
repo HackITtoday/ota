a8234587a46156dfc2257926ce43b4fedb2bc588
marcus hitchins <marcus7777@gmail.com>
2014-07-17 11:08:06 +0100

    added pay now if in CP APR

--- lib/find_rooms.php
+++ lib/find_rooms.php
@@ -520,7 +520,12 @@ function print_room($mapped, $Id, $Type, $num_rooms, $room, $title, $people_disp
         } 
       print '</span>';
     } 
-    print '<a title="' . $room['Formatted_cancellation_policy'] . '" class="cancelpolicy ui-link" href="#!">' . 'Cancellation policy';
+    print '<a title="' . $room['Formatted_cancellation_policy'] . '" class="cancelpolicy ui-link" href="#!">'; 
+    if (strpos($room['Formatted_cancellation_policy'] , 'advance purchase rate')) {
+      print  'Pay now ';
+    } else {
+      print  'Cancellation policy ';
+    }
     print '<span class="pay_at"></span>';
     print '<img width="10" height="10" src="/ota/inc/images/information.gif"> </a>';
     print '<div id="totprice">';
