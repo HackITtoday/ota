852b50014295b71cb35260e0dd3a28581ae46831
marcus Hitchins <marcus@hhost.me>
2014-07-24 19:06:58 +0100

    unrem  if count = 1 and added debugging afterw

--- lib/find_rooms.php
+++ lib/find_rooms.php
@@ -442,11 +442,16 @@ if ($ota == "1") { // verene
       print '<pre> $array[hotel][lr_rates][hotel_rooms] :' . print_r ($array['hotel']['lr_rates']['hotel_rooms']['room'],1) . '</pre>';
       print '<pre> count($array[hotel][lr_rates][hotel_rooms]) :' . print_r (count($array['hotel']['lr_rates']['hotel_rooms']),1) . '</pre>';
 }
-//if (count($array['hotel']['lr_rates']['hotel_rooms']) == 1){
-//  $temp = $array['hotel']['lr_rates']['hotel_rooms'];
-//  unset($array['hotel']['lr_rates']['hotel_rooms']);
-//  $array['hotel']['lr_rates']['hotel_rooms'] = $temp;
-//}
+if (count($array['hotel']['lr_rates']['hotel_rooms']) == 1){
+  $temp = $array['hotel']['lr_rates']['hotel_rooms'];
+  unset($array['hotel']['lr_rates']['hotel_rooms']);
+  $array['hotel']['lr_rates']['hotel_rooms']['room'] = $temp;
+}
+if ($_GET['debug'] == 2) {
+      print '<pre> $array[hotel][lr_rates][hotel_rooms] after :' . print_r ($array['hotel']['lr_rates']['hotel_rooms']['room'],1) . '</pre>';
+      print '<pre> count($array[hotel][lr_rates][hotel_rooms]) after :' . print_r (count($array['hotel']['lr_rates']['hotel_rooms']),1) . '</pre>';
+}
+
 ?>
 
 <center class="find-rooms">
